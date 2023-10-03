93b095f52ba3dab14e000defbeddba13f318ca45
Rex Rajat <rex.rajat@ithands.biz>
2023-10-03 12:22:59 +0530

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  # Specify the desired AWS provider version here
+    }
+  }
+}
+
 # Create a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
