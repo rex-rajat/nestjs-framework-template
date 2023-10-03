c95c0eef1fb18c66c59465362ce6c6de56e054cc
rex-rajat <94343221+rex-rajat@users.noreply.github.com>
2023-10-03 15:50:34 +0530

    Delete terraform/provider.tf

--- terraform/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12"  # Specify your desired Terraform version here
-}
-
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIA5VYTWOXCVSNRBP2O"
-  secret_key = "6L+y2VGTRoMfo2gTtOGkXkoZsVhVkcpx0w8lYy9T"
-}
